8be1f279d39b2087cf6e1fae6be6d881055c6b56
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2023-12-30 22:17:33 +0900

    setup function url app

--- app/index.py
+++ app/index.py
@@ -1,33 +1,65 @@
-import base64
-import typing
-
-from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import (
+    LambdaFunctionUrlResolver,
+    Response,
+)
+from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from jinja2 import Template
 from weasyprint import HTML
 
 logger = Logger()
+tracer = Tracer()
+
+app = LambdaFunctionUrlResolver()
 
 
-@logger.inject_lambda_context
-def lambda_handler(
-    event: dict[str, typing.Any],
-    context: LambdaContext,
-) -> dict[str, typing.Any]:
-    with open("invoice.tpl", "r") as fh:
-        template = "\n".join(fh.readlines())
+class Receipt:
+    def __init__(self) -> None:
+        pass
 
-    render_context = {
-        "invoice_date": "2000 年 1 月 23 日",
-        "invoice_number": "00000000-00000000-0000-0000",
-    }
+    def _generate_html(self) -> str:
+        with open("invoice.tpl", "r") as fh:
+            template = "\n".join(fh.readlines())
 
-    html_string = Template(source=template).render(render_context)
+        render_context = {
+            "invoice_date": "2000 年 1 月 23 日",
+            "invoice_number": "00000000-00000000-0000-0000",
+        }
 
-    HTML(string=html_string).write_pdf("/tmp/sample.pdf")
-    pdf_binary: bytes = HTML(string=html_string).write_pdf()
+        html_string = Template(source=template).render(render_context)
 
-    return {
-        "body": base64.b64encode(pdf_binary).decode(),
-        "isBase64Encoded": True,
-    }
+        return html_string
+
+    def print(self) -> bytes:
+        html = self._generate_html()
+
+        pdf_binary = HTML(string=html).write_pdf()
+
+        return pdf_binary
+
+
+@app.get(".+")
+@tracer.capture_method
+def get_pdf() -> Response:
+    return Response(
+        status_code=200,
+        content_type="application/pdf",
+        body=Receipt().print(),
+        headers={
+            "Content-Disposition": "; ".join(
+                [
+                    "attachment",
+                    'filename="example.pdf"',
+                ]
+            ),
+        },
+    )
+
+
+@logger.inject_lambda_context(
+    correlation_id_path=correlation_paths.LAMBDA_FUNCTION_URL,
+)
+@tracer.capture_lambda_handler
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
